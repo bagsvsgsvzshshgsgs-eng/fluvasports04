f7a4594c7daf896413c1bcdce62ef5f1c33c3d81
Sarah Abdelwahab <og1220329@gmail.com>
2026-05-14 15:53:48 +0300

    Fix Order schema price type to String

--- lib/models/Order.ts
+++ lib/models/Order.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const OrderItemSchema = new mongoose.Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  price: { type: Number, required: true },
+  price: { type: String, required: true },
   quantity: { type: Number, required: true },
   size: { type: String },
   color: { type: String },
